6e7b653a08c9414296d0f91264c898cdb3f8d50b
sameer <sameer@10.0.0.112>
2024-03-14 20:48:07 -0400

    Javadocs for tests.

--- LSP_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ LSP_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -1,6 +1,14 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * This class serves as a driver to test the functionality of the SecurityOps class.
+ */
 public class Driver {
+    /**
+     * The main method that executes the testing of the SecurityOps class.
+     *
+     * @param args command-line arguments (not used)
+     */
 	public static void main(String[] args) {
 		String text_string = "I love CSCI363";
 		System.out.println("The original string is: " + text_string + ", and the excrypted string is: " + SecurityOps.encrypt(text_string));
--- LSP_2024/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
+++ LSP_2024/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
@@ -1,19 +1,32 @@
 package org.howard.edu.lsp.midterm.question2;
 
+/**
+ * This class is used to test the functionality of the IntegerRange class.
+ */
+
 public class RangeTester {
+    /**
+     * The main method that executes the testing of the IntegerRange class.
+     *
+     * @param args command-line arguments (not used)
+     */
 	public static void main(String[] args) {
+        // Create two IntegerRange objects
+
 		IntegerRange range1 = new IntegerRange(4, 5);
 		Range range2 = new IntegerRange(5, 6);
 		
+        // Test equals and overlaps methods
 		System.out.println("Range 1: " + range1.toString() + " Range 2: " + range2.toString());
 		System.out.println("Range1 == Range2? " + range1.equals(range2));
 		System.out.println("Range1 overlaps Range2? " + range1.overlaps(range2));
 		
 		
+        // Modify range1 to have the same bounds as range2
 		range1.setLowerBound(5);
 		range1.setUpperBound(6);
 		
-		
+        // Test equals method after modification
 		System.out.println("Range 1: " + range1.toString() + " Range 2: " + range2.toString());
 		System.out.println("Range1 == Range2? " + range1.equals(range2));
 		
